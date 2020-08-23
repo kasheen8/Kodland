51de6567321bec957f396f7029821b112a20dea6
kasheen8 <abinaevravil@mail.ru>
2020-08-23 17:42:44 +0400

    Model "Post" created and PostgreSQL database connected

--- Kodland/settings.py
+++ Kodland/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +78,12 @@ WSGI_APPLICATION = 'Kodland.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Kodland',
+        'USER': 'postgres',
+        'PASSWORD': '6041061apa',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,18 @@
 from django.db import models
+from datetime import datetime
 
 class Post(models.Model):
     """Посты"""
-    
+    title = models.CharField("Название", max_length=150)
+    text = models.CharField("Текст", max_length=2000)
+    image = models.ImageField("Иллюстрация", upload_to="image/")
+    date = models.DateTimeField("Дата публикации", default=datetime.today())
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Пост"
+        verbose_name_plural = "Посты"
+        app_label = 'blog'
+
